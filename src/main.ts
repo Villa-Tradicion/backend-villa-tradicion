3517953df170d2c3d8bc5665de8977547d44b29e
AgnerVillaFabrega <adejesusvilla@unicesar.edu.co>
2025-04-06 00:45:32 -0500

    Add initial package [dotenv, joi, uuid, class-transformer, class-validator]

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Villa&Tradicion');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes( 
+    new ValidationPipe({ 
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+    }) 
+   )
+   
+  await app.listen(envs.port);
+  logger.log(`Running on port ${envs.port}`)
+}
+bootstrap();
